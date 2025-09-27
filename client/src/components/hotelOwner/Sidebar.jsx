9abdae6e0bed98328cd92ed2f02ec249c89b198c
kishan <kishangupta3003@gmail.com>
2025-09-27 16:41:16 +0530

    maked the sidebar stick

--- client/src/components/hotelOwner/Sidebar.jsx
+++ client/src/components/hotelOwner/Sidebar.jsx
@@ -11,7 +11,7 @@ const Sidebar = () => {
     ];
 
     return (
-        <div className="md:w-64 w-16 border-r h-screen text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
+        <div className="md:w-64 w-16 border-r h-screen sticky text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
             {sidebarLinks.map((item, index) => (
                 <NavLink to={item.path} key={index} end='/owner' className={({ isActive }) => `flex items-center py-3 px-4 md:px-8 gap-3 ${isActive ? "border-r-4 md:border-r-[6px] bg-blue-600/10 border-blue-600 text-blue-600" : "hover:bg-gray-100/90 border-white text-gray-700"}`}>
                     <img className="min-h-6 min-w-6" src={item.icon} alt={item.name} />
